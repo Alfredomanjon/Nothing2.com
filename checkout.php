1d74eac14ed542f3cada5ad253b6278ba4ebb416
Pablo Mendi <pablo.turmo@live.u-tad.com>
2019-05-10 14:36:57 +0200

    añadida carpeta imgs

--- checkout.php
+++ checkout.php
@@ -22,7 +22,7 @@
     ?>
     <div class="container">
       <div class="py-5 text-center">
-        <img class=" mx-auto mb-4" src="https://www.iconsdb.com/icons/preview/royal-blue/balloon-3-xxl.png" alt="" width="72" height="72">
+        <img class="d-block mx-auto mb-4" src="imgs/balloon-3-xxl.png" alt="" width="72" height="72">
         <h2>Checkout form</h2>
         <p class="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
       </div>
