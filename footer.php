932bba2ea61160f23f6df21576437b9098bf91fc
Pablo Mendi <pablo.turmo@live.u-tad.com>
2019-06-11 18:18:56 +0200

    footer changes

--- footer.php
+++ footer.php
@@ -4,27 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <!-- links footer -->
-    <link rel="stylesheet" href="css/footer.css">
-    <script src="js/footer.js"></script>
-    <title>footer</title>
+    <link rel="stylesheet" href="footer.css">
+    <title>Document</title>
 </head>
 <body>
-    <!-- Footer -->
-<footer >
-<div id="footer">
-    <div class="container">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 animated jackInTheBox text-center">
-                    <a href="index.php"> <h1>Nothing 2 buy</h1> </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+<footer class="page-footer font-small unique-color-dark pt-4">
+  <div class="container" id="footer">
+    <ul class="list-unstyled list-inline text-center py-2">
+      <li class="list-inline-item">
+        <h5 class="mb-1">Register for free</h5>
+      </li>
+      <li class="list-inline-item">
+        <a href="#!" class="btn btn-outline-white btn-rounded">Sign up!</a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
 </footer>
-<!-- Footer -->
 </body>
 </html>
-
