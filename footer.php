dd594c02ab059aa06f60b2c2fbeda35ca6b8d602
Pablo Mendi <pablo.turmo@live.u-tad.com>
2019-06-11 19:21:33 +0200

    no es un footer de niños

--- footer.php
+++ footer.php
@@ -4,25 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="footer.css">
+    <link rel=StyleSheet href="css/footer.css" type="text/css" media=screen>
     <title>Document</title>
 </head>
 <body>
-
-<footer class="page-footer font-small unique-color-dark pt-4">
-  <div class="container" id="footer">
-    <ul class="list-unstyled list-inline text-center py-2">
-      <li class="list-inline-item">
-        <h5 class="mb-1">Register for free</h5>
-      </li>
-      <li class="list-inline-item">
-        <a href="#!" class="btn btn-outline-white btn-rounded">Sign up!</a>
-      </li>
-    </ul>
+  <div class="footer">
+    <div id="button"></div>
+      <div id="container">
+        <div id="cont">
+          <div class="footer_center">
+              <h1>You ain't done buying. <br> Are you?</h1>
+          </div>
+      </div>
+    </div>
   </div>
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
-  </div>
-</footer>
 </body>
 </html>
