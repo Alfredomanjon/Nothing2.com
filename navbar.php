edc4b83a410f31c801ee06d657c94588d8a31bc4
Pablo Mendi <pablo.turmo@live.u-tad.com>
2019-06-12 14:39:48 +0200

    scrolling nav function responsive

--- navbar.php
+++ navbar.php
@@ -76,7 +76,7 @@ if(isset($_SESSION['nombre'])){
 <header >
 <nav id="global-nav" class="nav navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php">
-    <img src="imgs/logo100px.png" width="140" class="logo2 site-title">
+    <img src="imgs/logo100px.png" width="110" class="logo2 site-title">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
